4383e2f3b375be0d6f05d002830c4afaea9e830d
Pateros Petros <el19826@mail.ntua.gr>
2020-11-20 16:46:52 +0200

    Updates

--- st/config.mk
+++ st/config.mk
@@ -31,7 +31,8 @@ STLDFLAGS = $(LIBS) $(LDFLAGS)
 #CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
 #LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
 #       `$(PKG_CONFIG) --libs fontconfig` \
-#       `$(PKG_CONFIG) --libs freetype2`
+#       `$(PKG_CONFIG) --libs freetype2` \
+#       `$(PKG_CONFIG) --libs harfbuzz`
 
 # compiler and linker
 # CC = c99
