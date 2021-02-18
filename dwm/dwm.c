5cb299717365cf65d32f06b7f0a0a4a4343491a2
Pateros Petros <el19826@mail.ntua.gr>
2021-02-18 15:16:46 +0200

    visual feedback for sticky clients

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -847,6 +847,8 @@ drawbar(Monitor *m)
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
+			if (m->sel->issticky)
+				drw_rect(drw, x + boxs, bh - boxw - boxs, boxw, boxw, 1, 0);
 		} else {
 			drw_setscheme(drw, scheme[SchemeInfoNorm]);
 			drw_rect(drw, x, 0, w, bh, 1, 1);
